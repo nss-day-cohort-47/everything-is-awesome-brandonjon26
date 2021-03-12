c4f8e9d2592da784b8ee37fc8854630be6605a52
brandonjon26 <brandonjon26@gmail.com>
2021-03-12 16:01:59 -0600

    I GOT MVP BABY

--- scripts/legos/LegoDetail.js
+++ scripts/legos/LegoDetail.js
@@ -4,19 +4,20 @@ export const LegoDetail = (brick) => {
 
   let block = `<section class="block-wrapper" style="background-color:#${brick.ColorHex}">
               <h3>Name: ${brick.LegoName.toUpperCase()}</h3>
-              <h4>Notes: ${brick.Notes}<h4>
+              <h4>Notes: ${brick.Notes}<h/4>
+              <h4>Material: ${brick.Material}<h/4>
               <div class="block-years">Manufactured ${brick.YearFrom} - ${brick.YearTo}</div>
-              </section>
               `;
   const link = brick.ColorstreamLinkImage;
   if (link) {
     //true? wrap the block in an a tag
     return `
-      <a href="${link}" target="_blank" style="color:#${invertColor(brick.ColorHex)}">
-				${block}
-			</a>`;
+                <a href="${link}" target="_blank" style="color:#${invertColor(brick.ColorHex)}">
+                ${block}
+                </a>
+                </section>`;
   } else {
     //false? return the block
-    return block;
+    return block +="</section>";
   }
 }
